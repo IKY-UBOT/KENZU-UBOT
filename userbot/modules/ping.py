60f2fa59616cf9871f0b6ad5c070187928918b70
IKY-UBOT <101038045+IKY-UBOT@users.noreply.github.com>
2022-03-16 17:30:40 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -101,7 +101,7 @@ async def _(pong):
     duration = (end - start).microseconds / 1000
     user = await pong.client.get_me()
     await ram.edit(
-        f"**⭐IKY-UBOT⭐**\n"
+        f"**⭐ᴋᴀᴄᴜɴɢ ɪᴋʏ⭐**\n"
         f"** ➠  Sɪɢɴᴀʟ   :** "
         f"`%sms` \n"
         f"** ➠  Bᴏᴛᴠᴇʀ  :** "
